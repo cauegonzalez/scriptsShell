6611e306443612fe8ab90badd60a0fc7aba1ab8d
Cauê Gonzalez <cauegonzalez@gdax.com.br>
2017-08-02 12:00:14 -0300

    Alterações para melhorar legibilidade e facilitar modificações futuras

--- geraLista.sh
+++ geraLista.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-rm ./exercicios.txt;
+# lista todos os arquivos, exclui diretórios e .mp3
 LIST_ARQ=$(ls --hide=*.mp3 | grep -v ^d);
-i=1;
+# define o arquivo de saída do script
+saida=./exercicios.txt
+
+# inicializa o arquivo de saída como vazio, para apagar qualquer coisa pré-existente
+echo "" > $saida
 
 for arquivo in $LIST_ARQ
 do
-    if [ $arquivo != 'exercicios.txt' ]; then
-        echo "Ex.: $i" >> ./exercicios.txt;
-        cat $arquivo >> ./exercicios.txt;
-        echo "\n\n=======\n\n" >> ./exercicios.txt;
-        i=$((i+1))
+    # ignora o arquivo de saída
+    if [ $arquivo != $saida ]; then
+        # define um cabeçalho para o exercício, utilizando o nome do arquivo com o exercício
+        echo "Ex.: $arquivo" >> $saida;
+        # envia para o arquivo de saída o conteúdo do exercício
+        cat $arquivo >> $saida;
+        # grava no arquivo de saída um padrão para facilitar a leitura, dividindo as soluções
+        echo "\n\n=======\n\n" >> $saida;
     fi
 done
